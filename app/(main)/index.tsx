75228247b200e8bc068e8678cfc4a6157910d472
gabcytn <cayetanogabriel03@gmail.com>
2024-10-07 15:42:32 +0800

    added is logged in checker in main app

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Platform,
+  SafeAreaView,
+} from "react-native";
+import React, { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 
 const Dashboard = () => {
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (!isLoggedIn) {
+        router.replace("../(auth)/login");
+      }
+    };
+    checkLoginStatus();
+  }, []);
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Text>Dashboard</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default Dashboard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
