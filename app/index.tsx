ad2b0997e6b6e8d685c67d4022f12e57cb3d3e88
gabcytn <cayetanogabriel03@gmail.com>
2024-10-07 15:29:58 +0800

    fixed relative path error

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect } from "expo-router";
 
 const Home = () => {
-  return <Redirect href={"(main)"} />;
+  return <Redirect href={"/(main)"} />;
 };
 
 export default Home;
