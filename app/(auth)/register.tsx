346428decce675165b19157ad0e5d42213245bcc
gabcytn <cayetanogabriel03@gmail.com>
2024-10-08 09:50:18 +0800

    added register screen's ui components

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   return (
     <SafeAreaView>
       <Text>Register</Text>
+      <View>
+        <TextInput placeholder="Username" onChangeText={setUsername} />
+        <TextInput
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={setPassword}
+        />
+        <TextInput
+          placeholder="Confirm password"
+          secureTextEntry
+          onChangeText={setConfirmPassword}
+        />
+      </View>
+      <TouchableOpacity>
+        <Text>Create Account</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
