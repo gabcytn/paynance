a6d7ef7ca8ec8a4c48be7e117920c6179b03974d
gabcytn <cayetanogabriel03@gmail.com>
2024-10-19 20:35:48 +0800

    Placed add button at the bottom

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -12,6 +12,7 @@ import React, { useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import colors from "@/colors";
 
 const Dashboard = () => {
   useEffect(() => {
@@ -39,7 +40,7 @@ const Dashboard = () => {
           }}
         />
       </SafeAreaView>
-      <Pressable>
+      <Pressable style={styles.addButton}>
         <Ionicons name="add" />
       </Pressable>
     </>
@@ -51,5 +52,14 @@ export default Dashboard;
 const styles = StyleSheet.create({
   container: {
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  addButton: {
+    position: "absolute",
+    bottom: 30,
+    right: 30,
+    backgroundColor: colors.mainColor,
+    width: 50,
+    height: 50,
+    borderRadius: 100,
   },
 });
