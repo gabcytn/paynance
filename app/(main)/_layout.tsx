a68d2482f1ed6d0bc9c2cc84ae5c2ae00907e81f
gabcytn <cayetanogabriel03@gmail.com>
2024-10-20 14:52:40 +0800

    Fixed tab colors

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tabs } from "expo-router";
-
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import colors from "@/colors";
 const MainTabs = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
@@ -8,12 +9,16 @@ const MainTabs = () => {
         name="index"
         options={{
           title: "Dashboard",
+          tabBarIcon: ({ focused }) => 
+						<TabBarIcon size={28} name={"home"} color={ focused ? colors.mainColor : colors.greyWhite}/>
         }}
       />
       <Tabs.Screen
         name="history"
         options={{
           title: "History",
+          tabBarIcon: ({ focused }) => 
+						<TabBarIcon size={28} name={"time"} color={ focused ? colors.mainColor : colors.greyWhite}/>
         }}
       />
     </Tabs>
