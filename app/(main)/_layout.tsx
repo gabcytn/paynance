252f8bac147e2d5d521af408a42174230f262b7a
gabcytn <cayetanogabriel03@gmail.com>
2024-10-20 14:27:34 +0800

    Add history tab in tabs

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -4,7 +4,18 @@ import { Tabs } from "expo-router";
 const MainTabs = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Dashboard",
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+        }}
+      />
     </Tabs>
   );
 };
