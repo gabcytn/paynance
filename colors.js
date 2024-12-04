b0f2c38ac4fb810d456625cccdd2a4d071d328a6
gabcytn <cayetanogabriel03@gmail.com>
2024-12-04 21:50:44 +0800

    Renamed to constants.js
    Included other constant values outside hex color strings

--- colors.js
+++
@@ -1,8 +0,0 @@
-const colors = {
-  offWhite: "#FAF9F6",
-  greyWhite: "#AAA",
-  softBlack: "#020202",
-  mainColor: "#45B69C",
-};
-
-export default colors;
---
+++ constants.js
@@ -0,0 +1,13 @@
+const constants = {
+  colors: {
+    offWhite: "#FAF9F6",
+    greyWhite: "#AAA",
+    softBlack: "#020202",
+    mainColor: "#45B69C",
+  },
+  names: {
+    appName: "Paynance",
+  },
+};
+
+export default constants;
