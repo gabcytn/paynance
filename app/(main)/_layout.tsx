526a446eb882eb4d1bb1459e5bf837237848ea4f
gabcytn <cayetanogabriel03@gmail.com>
2024-12-05 02:58:26 +0800

    Created the sqlite database schema on load

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import colors from "@/colors";
+import constants from "@/constants";
 const MainTabs = () => {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
@@ -9,18 +9,18 @@ const MainTabs = () => {
         name="index"
         options={{
           title: "Dashboard",
-					tabBarActiveTintColor: colors.mainColor,
-          tabBarIcon: ({ focused }) => 
-						<TabBarIcon size={25} name={"podium"} color={ focused ? colors.mainColor : colors.greyWhite}/>
+          tabBarActiveTintColor: constants.colors.mainColor,
+          tabBarIcon: ({ focused }) =>
+            <TabBarIcon size={25} name={"podium"} color={focused ? constants.colors.mainColor : constants.colors.greyWhite} />
         }}
       />
       <Tabs.Screen
         name="history"
         options={{
           title: "History",
-					tabBarActiveTintColor: colors.mainColor,
-          tabBarIcon: ({ focused }) => 
-						<TabBarIcon size={25} name={"time"} color={ focused ? colors.mainColor : colors.greyWhite}/>
+          tabBarActiveTintColor: constants.colors.mainColor,
+          tabBarIcon: ({ focused }) =>
+            <TabBarIcon size={25} name={"time"} color={focused ? constants.colors.mainColor : constants.colors.greyWhite} />
         }}
       />
     </Tabs>
